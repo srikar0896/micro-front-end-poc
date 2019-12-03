d41e0655818ad1168d08c7b4638618d3d784214a
srikar.ch <srikar.0896@gmail.com>
2019-12-03 12:00:32 +0530

    exact path

--- src/RoutesContainer.js
+++ src/RoutesContainer.js
@@ -1,21 +1,27 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Result } from 'ui-infra';
 import DispatchPlans from './components/DispatchPlans';
 import Stocks from './components/Stocks';
 
 console.log('react-router', Switch);
 
-const RoutesContainer = () => {
+const RoutesContainer = props => {
+  console.log(props);
   return (
     // eslint-disable-next-line react/jsx-filename-extension
-    <Switch>
-      <Route exact path="/">
-        <DispatchPlans ids={[1, 2, 3]} />
-      </Route>
-      <Route path="/stocks">
-        <Stocks />
-      </Route>
-    </Switch>
+    <>
+      <Route exact path={`${props.baseRoute}/dps`} component={DispatchPlans} />
+      <Route exact path={`${props.baseRoute}/stocks`} component={Stocks} />
+      <Route
+        exact
+        path="/404"
+        component={() => <Result title="Page not found" />}
+      />
+      <Redirect exact from="/" to={`${props.baseRoute}/dps`} />
+    </>
   );
 };
 
